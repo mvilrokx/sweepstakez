c18a5b003bae3a698f3491362f072081145c675c
Mark Vilrokx <mvilrokx@gmail.com>
2014-05-22 10:37:33 -0700

    changed the groupsService to use a resource that is now calling a Sinatra service which returns the groups from the server.  Still hardcoded on the server, but at least not anymore on the client.

--- app.rb
+++ app.rb
@@ -4,19 +4,31 @@ require 'sinatra/base'
 require 'ap'
 require 'json'
 # require 'sinatra/respond_to'
-require 'rest_client'
+# require 'rest_client'
 
 
 class App < Sinatra::Base
 
   # enable :sessions
 
+  groups = {
+    '2014 FIFA WORLD CUP' => {
+      'A' => ['Brazil', 'Croatia', 'Mexico', 'Cameroon'],
+      'B' => ['Spain', 'Netherlands', 'Chile', 'Australia'],
+      'C' => ['Colombia', 'Greece', 'Côte D\'Ivoire', 'Japan'],
+      'D' => ['Uruguay', 'Costa Rica', 'England', 'Italy'],
+      'E' => ['Switzerland', 'Ecuador', 'France', 'Honduras'],
+      'F' => ['Argentina', 'Bosnia and Herzegovina', 'Iran', 'Nigeria'],
+      'G' => ['Germany', 'Portugal', 'Ghana', 'USA'],
+      'H' => ['Belgium', 'Algeria', 'Russia', 'Korea Republic']
+    }
+  };
+
   configure do
   #   set :views, settings.root + '/app/views'
     enable :logging
     set :root, File.dirname(__FILE__)
     set :public_folder, ENV['RACK_ENV'] == 'production' ? 'public/dist' : 'public/app'
-    # set :styles_folder, ENV['RACK_ENV'] == 'production' ? 'public/dist/styles' : 'public/.tmp/styles';
   end
 
   before do
@@ -31,6 +43,11 @@ class App < Sinatra::Base
   get '/hello' do
     content_type :json
     { message: 'Hello World!' }.to_json
+  end
+
+  get '/groups/:tournament' do
+    content_type :json
+    groups[params[:tournament]].to_json
   end
 
   get '/' do
--- public/app/scripts/services/groupsService.js
+++ public/app/scripts/services/groupsService.js
@@ -1,22 +1,10 @@
 'use strict';
 
-app.service('groupsService', [function(){
-  var groups = {
-    '2014 FIFA WORLD CUP': {
-      A: ['Brazil', 'Croatia', 'Mexico', 'Cameroon'],
-      B: ['Spain', 'Netherlands', 'Chile', 'Australia'],
-      C: ['Colombia', 'Greece', 'Côte D\'Ivoire', 'Japan'],
-      D: ['Uruguay', 'Costa Rica', 'England', 'Italy'],
-      E: ['Switzerland', 'Ecuador', 'France', 'Honduras'],
-      F: ['Argentina', 'Bosnia and Herzegovina', 'Iran', 'Nigeria'],
-      G: ['Germany', 'Portugal', 'Ghana', 'USA'],
-      H: ['Belgium', 'Algeria', 'Russia', 'Korea Republic']
-    }
-  };
+app.service('groupsService', ['$resource', function($resource){
+  var groups = $resource('/groups/:tournament');
 
   this.getGroups = function(tournament){
-    return groups[tournament];
+    return groups.get({tournament: tournament});
   };
 
 }]);
-
